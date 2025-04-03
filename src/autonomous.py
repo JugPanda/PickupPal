4612865942db841d45d5579369d2ea432cd7fb9f
JugPanda <144568069+JugPanda@users.noreply.github.com>
2025-04-02 23:01:12 -0400

    Update autonomous.py
    Updated file to Object Oriented format

--- src/autonomous.py
+++ src/autonomous.py
@@ -1,40 +1,76 @@
-from gpiozero import Robot, DistanceSensor
-#from picamera2 import Picamera2, Preview
-#from signal import pause
+from gpiozero import Robot, Motor, DistanceSensor, PWMOutputDevice
+from picamera2 import Picamera2, Preview
 from time import sleep
+import curses
 
 
-#picam = Picamera2()
-robot = Robot(left=(6, 5), right=(16, 18))
-#sensor = DistanceSensor(echo=23, trigger=24)
-#distance = sensor.value * 100
-time = 0
+class PickupPal:
+    def __init__(self):
+        # Left motor
+        self.left_motor = Motor(forward=6, backward=5)
+        self.left_pwm = PWMOutputDevice(18)
+        self.left_pwm.value = 0
 
-#picam.start_preview(Preview.QTGL)
-#picam.start()
-class PickupPal():
-	def __init__(self):
-		self.left_motor = Motor(forward=6, backward=5)
-		self.left_pwm = PWMOutputDevice(18)
-		self.left_pwm.value = 0
+        # Right motor
+        self.right_motor = Motor(forward=16, backward=18)
+        self.right_pwm = PWMOutputDevice(18)
+        self.right_pwm.value = 0
+        self.sensor = DistanceSensor(echo=23, trigger=24)
+        self.distance = self.sensor.value * 100
 
-	def forward(self):
-		self.left_motor.forward()
-		self.left_pwm.value = 0.5
+        # Raspberry Pi camera
+        self.picam = Picamera2()
+        self.picam.start_preview(Preview.QTGL)
+        self.picam.start()
 
-	def backward(self):
-		self.
+    def forward(self):
+        self.left_motor.forward()
+        self.left_pwm.value = 0.5
 
-while time < 5:
-#	distance = sensor.value * 100
+    def backward(self):
+        self.left_motor.backward()
+        self.left_pwm.value = 0.5
 
-#	print(distance)
-#	if distance < 30:
-#		robot.stop()
-#	else:
-	print("drive")
-	robot.backward()
-	sleep(1)
-	time += 1
+    def map_key_to_command(self, key):
+        mapa = {curses.KEY_UP: self.forward, curses.KEY_DOWN: self.backward}
+        return mapa[key]
 
-#picam.close()
+    def control(self, key):
+        return self.map_key_to_command(key)
+
+
+gpio_pal = PickupPal()
+
+
+def main(time, window):
+    next_key = None
+    while time < 60:
+        if next_key is None:
+            key = window.getch()
+            print(key)
+        else:
+            key = next_key
+            next_key = None
+        if key != 1:
+            # Key was pressed
+            curses.halfdelay(1)
+            action = gpio_pal.control(key)
+            if action:
+                action()
+            next_key = key
+            while next_key == key:
+                next_key = window.getch()
+            # Key was released
+            gpio_pal.left_motor.stop()
+
+        if gpio_pal.distance < 30:
+            gpio_pal.backward()
+        else:
+            gpio_pal.forward()
+        robot.backward()
+        sleep(1)
+        time += 1
+
+
+# picam.close()
+curses.wrapper(main)
